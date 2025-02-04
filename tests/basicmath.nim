d4ea0413bfed051c748b898689ea3a3d61b411c3
Constantine Molchanov <moigagoo@duck.com>
2025-02-04 15:34:31 +0400

    Add test for multiarg command.

--- tests/basicmath.nim
+++ tests/basicmath.nim
@@ -6,18 +6,39 @@ import climate
 proc sum(context: Context): int =
   context.args:
     if len(args) != 2:
-      echo "The command expects exactly two arguments"
+      echo "The command expects exactly two arguments."
       return 1
 
     echo parseInt(args[0]) + parseInt(args[1])
 
   do:
-    echo "The command expects exactly two arguments"
+    echo "The command expects exactly two arguments."
     return 2
+
+
+proc multisum(context: Context): int =
+  var accum = 0
+
+  context.args:
+    for arg in args:
+      accum += parseInt(arg)
+
+  do:
+    echo "The command expects at least one argument."
+    return 2
+
+  echo accum
+
 
 proc root(context: Context): int =
   echo "Available commands: sum."
 
 
-quit parseCommands({"sum": sum}, defaultHandler = root)
+const commands = {
+  "sum": sum,
+  "multisum": multisum,
+}
+
+
+quit parseCommands(commands, defaultHandler = root)
 
--- tests/tbasicmath.nim
+++ tests/tbasicmath.nim
@@ -6,7 +6,7 @@ const clifile = "tests" / "basicmath"
 suite "Basic CLI":
   discard execCmd("nim c " & clifile)
 
-  test "Run command with two arguments":
+  test "Two arguments expected, two given":
     const
       a = 2
       b = 3
@@ -16,7 +16,7 @@ suite "Basic CLI":
     check code == 0
     check parseInt(strip(output)) == a + b
 
-  test "Run command with too many arguments":
+  test "Two arguments expected, three given":
     const
       a = 2
       b = 3
@@ -25,15 +25,41 @@ suite "Basic CLI":
     let (output, code) = execCmdEx &"{findExe(clifile)} sum {a} {b} {c}"
 
     check code == 1
-    check (strip(output)) == "The command expects exactly two arguments"
+    check (strip(output)) == "The command expects exactly two arguments."
 
-  test "Run command without arguments":
+  test "Two arguments expected, none given":
     let (output, code) = execCmdEx &"{findExe(clifile)} sum"
 
     check code == 2
-    check (strip(output)) == "The command expects exactly two arguments"
+    check (strip(output)) == "The command expects exactly two arguments."
 
-  test "Run without command":
+  test "Multiple arguments expected, multiple given":
+    const
+      a = 2
+      b = 3
+      c = 4
+
+    let (output, code) = execCmdEx &"{findExe(clifile)} multisum {a} {b} {c}"
+
+    check code == 0
+    check parseInt(strip(output)) == a + b + c
+
+  test "Multiple arguments expected, one given":
+    const
+      a = 2
+
+    let (output, code) = execCmdEx &"{findExe(clifile)} multisum {a}"
+
+    check code == 0
+    check parseInt(strip(output)) == a
+
+  test "Multiple arguments expected, none given":
+    let (output, code) = execCmdEx &"{findExe(clifile)} multisum"
+
+    check code == 2
+    check (strip(output)) == "The command expects at least one argument."
+
+  test "Command missing":
     let (output, code) = execCmdEx &"{findExe(clifile)}"
 
     check code == 0
